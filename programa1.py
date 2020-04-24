bb04ee3513b8fabc9a753b529835654011f264e0
Carlos1Daniel2Sanchez3 <64233853+Carlos1Daniel2Sanchez3@users.noreply.github.com>
2020-04-24 15:44:59 -0500

    Add files via upload
    se compila dandole solo en run

---
+++ programa1.py
@@ -0,0 +1,12 @@
+import modulo1
+
+coche1 = modulo1.Coche("jetta","rojo","diesel","1.8")
+
+print(coche1.mostrar_caracteristicas())
+
+media = modulo1.media(3,8,7)
+
+print("Nuestra nota media es {}".format(media))
+
+
+
